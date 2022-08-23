9a5fec7692c82edf99e18c93a48c07a435492205
Maxim <maximkachmaryk12@gmail.com>
2022-08-23 18:42:33 +0000

    add data to js

---
+++ script.js
@@ -0,0 +1,24 @@
+(function () {
+    const productTab = [
+        {
+            id: '1',
+            title: 'SUVs',
+            tabs__subtitle: 'No obstacles on your way<br> can spoil the trip',
+                image: 'img/tabs-with-cars/ml.png'
+        },
+        {
+        id: '2',
+        title: 'Sports Cars',
+        tabs__subtitle: 'Feel what true speed<br> and freedom are.',
+        image: 'img/tabs-with-cars/ml.png'
+    },
+    
+    {
+        id: '3',
+        title: 'Luxury',
+        tabs__subtitle: 'Comfortable cars for<br> demanding clients.',
+        image: 'img/tabs-with-cars/ml.png'
+    }
+
+    ]
+})
