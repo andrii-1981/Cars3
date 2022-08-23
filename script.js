b8f8aeb628c4ef3abc2ff80cce5ab3a8699ff68d
Maxim <maximkachmaryk12@gmail.com>
2022-08-23 19:04:01 +0000

    add renders function

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
+//IIFI
 (function () {
     const productTab = [
         {
             id: '1',
             title: 'SUVs',
             tabs__subtitle: 'No obstacles on your way<br> can spoil the trip',
-                image: 'img/tabs-with-cars/ml.png'
+            image: 'img/tabs-with-cars/ml.png'
         },
         {
-        id: '2',
-        title: 'Sports Cars',
-        tabs__subtitle: 'Feel what true speed<br> and freedom are.',
-        image: 'img/tabs-with-cars/ml.png'
-    },
-    
-    {
-        id: '3',
-        title: 'Luxury',
-        tabs__subtitle: 'Comfortable cars for<br> demanding clients.',
-        image: 'img/tabs-with-cars/ml.png'
-    }
+            id: '2',
+            title: 'Sports Cars',
+            tabs__subtitle: 'Feel what true speed<br> and freedom are.',
+            image: 'img/tabs-with-cars/ml.png'
+        },
 
-    ]
-})
+        {
+            id: '3',
+            title: 'Luxury',
+            tabs__subtitle: 'Comfortable cars for<br> demanding clients.',
+            image: 'img/tabs-with-cars/ml.png'
+        }
+
+    ];
+
+    function renderTabs(Tabs) {
+        const tabsContainer = document.querySelector('tabs');
+        tabsContainer.innerHTML = '';
+        for (productTab of Tabs) {
+            tabsContainer.innerHTML.innerHTML += `
+            <div class="tab">
+                        <div class="tab__img"> <img src="img/tabs-with-cars/ml.png" alt="carML">
+                        </div>
+                        <h2 class="tabs__title">SUVs</h2>
+                        <h3 class="tabs__subtitle">No obstacles on your way<br> can spoil the trip.</h3>
+                        <button class="tabs__button">LEARN MORE</button>
+                    </div>`
+        }
+    }
+})();
