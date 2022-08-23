b300e87f053e77ec34b7a713aed0867fd5e3dd87
Maxim <maximkachmaryk12@gmail.com>
2022-08-23 19:51:17 +0000

    formatting js

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
-//IIFI
 (function () {
-    const productTab = [
-        {
-            id: '1',
-            title: 'SUVs',
-            tabs__subtitle: 'No obstacles on your way<br> can spoil the trip',
-            image: 'img/tabs-with-cars/ml.png'
-        },
-        {
-            id: '2',
-            title: 'Sports Cars',
-            tabs__subtitle: 'Feel what true speed<br> and freedom are.',
-            image: 'img/tabs-with-cars/ml.png'
-        },
+  const tabs = [
+    {
+      id: "1",
+      title: "SUVs",
+      tabs__subtitle: "No obstacles on your way<br> can spoil the trip",
+      image: "img/tabs-with-cars/ml.png",
+      button: Learnmore,
+    },
+    {
+      id: "2",
+      title: "Sports Cars",
+      tabs__subtitle: "Feel what true speed<br> and freedom are.",
+      image: "img/tabs-with-cars/ml.png",
+      button: Learnmore,
+    },
 
-        {
-            id: '3',
-            title: 'Luxury',
-            tabs__subtitle: 'Comfortable cars for<br> demanding clients.',
-            image: 'img/tabs-with-cars/ml.png'
-        }
+    {
+      id: "3",
+      title: "Luxury",
+      tabs__subtitle: "Comfortable cars for<br> demanding clients.",
+      image: "img/tabs-with-cars/ml.png",
+      button: Learnmore,
+    },
+  ];
 
-    ];
+  function renderTabs(tabs) {
+    const tabsContainer = document.querySelector('.prods');
+    tabsContainer.innerHTML = '';
+    for ( const product of tabs) {
+        tabsContainer.innerHTML.innerHTML += `
+        <div class="tab">
+                   <img class="tab__img" src="${product.image}" alt="${product.title}">
+                    <h2 class="tabs__title">${'.title'}</h2>
+                    <h3 class="tabs__subtitle">${product.tabs__subtitle}</h3>
+                    <button class="tabs__button">${product.button}</button>
+         </div>`;
 
-    function renderTabs(Tabs) {
-        const tabsContainer = document.querySelector('tabs');
-        tabsContainer.innerHTML = '';
-        for (productTab of Tabs) {
-            tabsContainer.innerHTML.innerHTML += `
-            <div class="tab">
-                        <div class="tab__img"> <img src="img/tabs-with-cars/ml.png" alt="carML">
-                        </div>
-                        <h2 class="tabs__title">SUVs</h2>
-                        <h3 class="tabs__subtitle">No obstacles on your way<br> can spoil the trip.</h3>
-                        <button class="tabs__button">LEARN MORE</button>
-                    </div>`
-        }
     }
+}
+  renderTabs(tabs);
 })();
