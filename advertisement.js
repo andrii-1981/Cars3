f99b5597b357ea969b829a6fc8430a02c462ad4f
[Maxym Kachmaryk] <[maximkachmaryk12@gmail.com]>
2022-08-29 16:07:10 +0300

    clone

--- advertisement.js
+++ advertisement.js
@@ -1,33 +1,33 @@
-(function () {
-  const products = [
-    {
-      id: "1",
-      title: "SUVs",
-      tabs__subtitle: "No obstacles on your way<br> can spoil the trip",
-      image: "img/tabs-with-cars/ml.png",
-      btn:"LEARN MORE"
-    },
-    {
-      id: "2",
-      title: "Sports Cars",
-      tabs__subtitle: "Feel what true speed<br> and freedom are.",
-      image: "img/tabs-with-cars/nissan.png",
-      btn:"LEARN MORE"
-    },
-    {
-      id: "3",
-      title: "Luxury",
-      tabs__subtitle: "Comfortable cars for<br> demanding clients.",
-      image: "img/tabs-with-cars/mersedes.png",
-      btn: "LEARN MORE"
-    }
-  ];
+(function() {
+    const products = [{
 
-  function renderCards(products) {
-    const tabsContainer = document.querySelector('.cards');
-    tabsContainer.innerHTML = '';
-    for (const product of products) {
-        tabsContainer.innerHTML +=`
+            id: "1",
+            title: "SUVs",
+            tabs__subtitle: "No obstacles on your way<br> can spoil the trip",
+            image: "img/tabs-with-cars/ml.png",
+            btn: "LEARN MORE"
+        },
+        {
+            id: "2",
+            title: "Sports Cars",
+            tabs__subtitle: "Feel what true speed<br> and freedom are.",
+            image: "img/tabs-with-cars/nissan.png",
+            btn: "LEARN MORE"
+        },
+        {
+            id: "3",
+            title: "Luxury",
+            tabs__subtitle: "Comfortable cars for<br> demanding clients.",
+            image: "img/tabs-with-cars/mersedes.png",
+            btn: "LEARN MORE"
+        }
+    ];
+
+    function renderCards(products) {
+        const tabsContainer = document.querySelector('.cards');
+        tabsContainer.innerHTML = '';
+        for (const product of products) {
+            tabsContainer.innerHTML += `
         <div class="card">
                    <img class="card__img" src="${product.image}" alt="${products.title}">
                     <h2 class="cards__title">${product.title}</h2>
@@ -38,7 +38,7 @@
                    
          </div>`;
 
+        }
     }
-}
-  renderCards(products);
+    renderCards(products);
 })();
